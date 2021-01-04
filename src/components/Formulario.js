5e225a864a984c8a5169dfe30ce3947599706cc9
Jean Paul Gotopo Maldonado <jpgotopo@GMAIL.COM>
2021-01-03 23:10:01 -0500

    Actualizando importación uuid
    Me presentaba fallas y veía que era en esta línea, consulté y esta es la soución: import {v4 as uuid} from "uuid";

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import uuid from 'uuid/v4';
+import {v4 as uuid} from "uuid";
 import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
